e13aed0800f2d156c9dc733fd2a7443cc69feaed
Vinjar Vederhus <vinjar97@gmail.com>
2021-06-22 12:50:47 +0200

    Added unprintability value in gcode filename

--- autoslice.py
+++ autoslice.py
@@ -17,9 +17,13 @@ class AutoSlicer:
     def __tweakFile(self, inputFile, dir):
         try:
             outputFile = dir + "\\tweaked.stl"
-            subprocess.run(["python", self.tweakerPath + "\\Tweaker.py", "-i", inputFile, "-o", outputFile, "-x"])
+            result = subprocess.run(["python", self.tweakerPath + "\\Tweaker.py", "-i", inputFile, "-o", outputFile, "-x", "-vb"]
+                                    , capture_output=True, text=True).stdout
+            _, temp = result.splitlines()[-5].split(":")
+            unprintability = str(round(float(temp.strip()), 2))
+            print("Unprintability: " + unprintability)
             print(outputFile)
-            return outputFile
+            return outputFile, unprintability
         except:
             print("Couldn't run tweaker on file " + self.inputFile)
 
@@ -39,9 +43,9 @@ class AutoSlicer:
             print("Couldn't adjust height of file " + self.inputFile)
 
 
-    def __runSlicer(self, inputFile, dir, initialName):
+    def __runSlicer(self, inputFile, dir, initialName, unprintability):
         [filename, extension] = initialName.split(".")
-        outputFile = dir + "\\" + filename + ".gcode"
+        outputFile = dir + "\\" + filename + "U" + unprintability + ".gcode"
         try:
             subprocess.run([self.slicerPath, "--load", self.configPath
                             , "-g", "-o", outputFile, inputFile])
@@ -52,36 +56,6 @@ class AutoSlicer:
         self.inputFile = input
         with tempfile.TemporaryDirectory() as tempDirectory:
             print(tempDirectory)
-            tweakedFile = self.__tweakFile(self.inputFile, tempDirectory)
+            tweakedFile, unprintability = self.__tweakFile(self.inputFile, tempDirectory)
             translatedFile = self.__adjustHeight(tweakedFile, tempDirectory)
-            self.__runSlicer(translatedFile, outputPath, initialName)
-        
-
-
-
-
-# inFile = "C:\\Users\\vinja\\Downloads\\BagClip_65.stl
-# tweakedFile = "C:\\Users\\vinja\\Downloads\\tweakedMesh.stl"
-# translatedFile = "C:\\Users\\vinja\\Downloads\\movedMesh.stl"
-# finishedFile = "C:\\Users\\vinja\\Downloads\\output.gcode"
-
-# subprocess.run(["python", tweakerPath + "\\Tweaker.py", "-i", inFile, "-o", tweakedFile, "-x"])
-
-# myMesh = Mesh.from_file(tweakedFile)
-# print("Z min:", myMesh.z.min())
-# print("Z max:", myMesh.z.max())
-# translation = np.array([0, 0, -myMesh.z.min()])
-# myMesh.translate(translation)
-# print("Translated, new Z min:", myMesh.z.min())
-# myMesh.save(translatedFile)
-
-# subprocess.run([slicerPath, "--load", "C:\\Users\\vinja\\Downloads\\MK3Sconf.ini", "-g", "-o", finishedFile, translatedFile])
-
-# client = OctoRest(url="http://octopi05.local", apikey="9159AB574FE84EB39B8C6760C23CDF2E")
-# state = client.state()
-# if (state == "Operational"):
-#     client.upload(finishedFile)
-#     client.select("output.gcode", print=True)
-#     print("Print started on printer 05")
-# else:
-#     print("Printer not available, finished")
+            self.__runSlicer(translatedFile, outputPath, initialName, unprintability)
