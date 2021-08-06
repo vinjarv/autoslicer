2c0602ea12d66762ed7c636214a35ab3414383d3
Vinjar Vederhus <vinjar97@gmail.com>
2021-08-06 10:03:33 +0200

    Get absolute paths

--- autoslice.py
+++ autoslice.py
@@ -2,6 +2,7 @@ import subprocess
 import tempfile
 
 import numpy as np
+import os
 from stl import Mesh
 
 class AutoSlicer:
@@ -13,10 +14,11 @@ class AutoSlicer:
     treshold_brim = 2.0
 
 
-    def __tweakFile(self, inputFile, dir):
+    def __tweakFile(self, input_file, dir):
         try:
-            outputFile = dir + "\\tweaked.stl"
-            result = subprocess.run(["python", self.config["PATHS"]["tweaker"] + "\\Tweaker.py", "-i", inputFile, "-o", outputFile, "-x", "-vb"]
+            outputFile = os.path.join(dir, "tweaked.stl")
+            print(outputFile)
+            result = subprocess.run(["python", os.path.join(self.config["PATHS"]["tweaker"], "Tweaker.py"), "-i", input_file, "-o", outputFile, "-x", "-vb"]
                                     , capture_output=True, text=True).stdout
             _, temp = result.splitlines()[-5].split(":")
             unprintability = str(round(float(temp.strip()), 2))
@@ -25,49 +27,68 @@ class AutoSlicer:
             print(outputFile)
             return outputFile, unprintability
         except:
-            print("Couldn't run tweaker on file " + self.inputFile)
+            print("Couldn't run tweaker on file " + self.input_file)
 
 
-    def __adjustHeight(self, inputFile, dir):
+    def __adjustHeight(self, input_file, dir):
         try:
-            outputFile = dir + "\\translated.stl"
-            myMesh = Mesh.from_file(inputFile)
-            print("Z min:", myMesh.z.min())
-            print("Z max:", myMesh.z.max())
-            translation = np.array([0, 0, -myMesh.z.min()])
-            myMesh.translate(translation)
-            print("Translated, new Z min:", myMesh.z.min())
-            myMesh.save(outputFile)
-            return outputFile
+            output_file = os.path.join(dir, "translated.stl")
+            my_mesh = Mesh.from_file(input_file)
+            print("Z min:", my_mesh.z.min())
+            print("Z max:", my_mesh.z.max())
+            translation = np.array([0, 0, -my_mesh.z.min()])
+            my_mesh.translate(translation)
+            print("Translated, new Z min:", my_mesh.z.min())
+            my_mesh.save(output_file)
+            return output_file
         except:
-            print("Couldn't adjust height of file " + self.inputFile)
+            print("Couldn't adjust height of file " + self.input_file)
 
 
-    def __runSlicer(self, inputFile, initialName, unprintability):
-        filename, _ = initialName.split(".")
+    def __runSlicer(self, input_file, initial_name, unprintability):
+        filename, _ = initial_name.split(".")
         filename = filename.replace(" ", "_") # Remove spaces
-        outputFile = self.config["PATHS"]["outputDirectory"] + "\\" + filename + "_U" + str(unprintability) + "_{print_time}" ".gcode"
 
-        cmd = [self.config["PATHS"]["slicer"], "--load", self.config["PATHS"]["slicerConfig"]]
+        cwd = os.getcwd()
+
+        input_file = os.path.join(
+            cwd,
+            input_file
+        )
+        output_file = os.path.join(
+            cwd,
+            self.config["PATHS"]["outputDirectory"],
+            (filename + "_U" + str(unprintability) + "_{print_time}" ".gcode")
+            )
+        slicer_loc = os.path.join(
+            cwd,
+            self.config["PATHS"]["slicer"]
+        )
+        config_loc = os.path.join(
+            cwd,
+            self.config["PATHS"]["slicerConfig"]
+        )
+
+        cmd = [slicer_loc, "--load", config_loc]
 
         if float(unprintability) > self.treshold_brim:
             cmd.extend(["--brim-width", "5", "--skirt-distance", "6"])
         if float(unprintability) > self.treshold_supports:
             cmd.append("--support-material")
 
-        cmd.extend(["-g", "-o", outputFile, inputFile])
+        cmd.extend(["-g", "-o", output_file, input_file])
         print(cmd)
         try:
             subprocess.run(cmd)
         except:
-            print("Couldn't slice file " + self.inputFile)
+            print("Couldn't slice file " + self.input_file)
 
 
-    def slice(self, input, configParsed, initialName):
-        self.inputFile = input
-        self.config = configParsed
-        with tempfile.TemporaryDirectory() as tempDirectory:
-            print(tempDirectory)
-            tweakedFile, unprintability = self.__tweakFile(self.inputFile, tempDirectory)
-            translatedFile = self.__adjustHeight(tweakedFile, tempDirectory)
-            self.__runSlicer(translatedFile, initialName, unprintability)
+    def slice(self, input, config_parsed, initial_name):
+        self.input_file = input
+        self.config = config_parsed
+        with tempfile.TemporaryDirectory() as temp_directory:
+            print(temp_directory)
+            tweakedFile, unprintability = self.__tweakFile(self.input_file, temp_directory)
+            translatedFile = self.__adjustHeight(tweakedFile, temp_directory)
+            self.__runSlicer(translatedFile, initial_name, unprintability)
